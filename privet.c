58d82fe6f6584ec42479a2d43e71141356bedf63
Dmitry <diepress@gmail.com>
2015-12-12 21:09:43 +0200

    multi add

--- privet.c
+++ privet.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char* argv[])
 {
-    printf("%s, %s\n", "Hello", "denis");
+    printf("%s, %s\n", "Hello", "Denis");
     return 0;
 }
--- start.cpp
+++ start.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[])
 {
 
-    std::cout << "Hello, denis!" << std::endl;
+    std::cout << "Hello, denis !" << std::endl;
     return 0;
 }
